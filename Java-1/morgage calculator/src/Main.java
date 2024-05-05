83e5257fe52e9e2f5388d8798a176dc52a7354cb
AndrewArcherERA <a.archer@erasolutions.us>
2024-05-05 12:24:39 -0400

    asdf

--- Java-1/morgage calculator/src/Main.java
+++ Java-1/morgage calculator/src/Main.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int principle;
+        double annualInterest;
+        int period;
+        while(true){
+            System.out.print("Principle ($1K - $1M): ");
+            principle = scanner.nextInt();
+            if(principle >= 1000 && principle <= 1000000) break;
+            else System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
 
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextDouble();
+            if(annualInterest > 0 && annualInterest <= 30) break;
+            else System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        double annualInterest = scanner.nextDouble();
+        while(true){
+            System.out.print("Period (Years): ");
+            period = scanner.nextInt();
+            if(period >= 1 && period <= 30) break;
+            else System.out.println("Enter a period between 1 and 30.");
+        }
+
         double monthlyInterest = (annualInterest / 100) / 12;
-
-        System.out.print("Period (Years): ");
-        int period = scanner.nextInt();
         int numPayments = period * 12;
 
         double x = monthlyInterest * (Math.pow((1 + monthlyInterest), numPayments));

b012c8b2f5b5ae805366bd5c7043a1af7e1cc01e
AndrewArcherERA <a.archer@erasolutions.us>
2024-05-06 22:46:47 -0400

    asd

---
+++ Java-2/src/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public Employee(int baseSalary, int hourlyRate) {
+        setHourlyRate(hourlyRate);
+        setBaseSalary(baseSalary);
+    }
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
+    public int calculateWage(){
+        return calculateWage(0);
+    }
+
+    private void setBaseSalary(int baseSalary){
+        if(baseSalary <= 0) throw new IllegalArgumentException("Salary can not be less than 0");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary(){
+        return baseSalary;
+    }
+
+    private void setHourlyRate(int hourlyRate){
+        if(hourlyRate <= 0) throw new IllegalArgumentException("Hourly rate can not be 0 or less.");
+        this.hourlyRate = hourlyRate;
+    }
+
+    private int getHourlyRate(){
+        return hourlyRate;
+    }
+}
